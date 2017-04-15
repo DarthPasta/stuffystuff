f82f140179f263154b696e0e3e953b2ffbb1fd6c
Liow Jen Nick <liowj.nick@gmail.com>
2017-04-15 08:10:45 +0000

    Added test for books controller

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionController::TestCase
+  test "should get index" do 
+    get :index 
+    assert_response :success
+    assert_not_nil assigns(:books)
+  end
   # test "the truth" do
   #   assert true
   # end
